cb9cf6205a8971c58bc389b8ccb1535e659f9038
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-12-25 04:51:26 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import App1 from 'components/App1';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import  App  from 'components/App';
+// import  App  from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App1/>
   </React.StrictMode>
 );
